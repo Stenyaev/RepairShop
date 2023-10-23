f87df4f9d01fab90ec408b23abecde93592c841f
steni <steniaev.dmitry@gmail.com>
2023-10-23 13:13:54 +0300

    1.1

--- src/main/java/ru/stenyaev/RepairShop/repo/RoleRepo.java
+++ src/main/java/ru/stenyaev/RepairShop/repo/RoleRepo.java
@@ -2,10 +2,11 @@ package ru.stenyaev.RepairShop.repo;
 
 
 
+import org.springframework.data.repository.CrudRepository;
 import ru.stenyaev.RepairShop.entity.Role;
 
 import java.util.Optional;
 
-public interface RoleRepo {
+public interface RoleRepo extends CrudRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
